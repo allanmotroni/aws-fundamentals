4d64774cfcc09eb4bb1837d30d019c54ca4601b1
Allan Motroni <allan.motroni@hotmail.com>
2024-08-31 16:44:56 -0300

    feat: create project and create sqs publisher

---
+++ SqsPublisher/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using SqsPublisher;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+var sqsClient = new AmazonSQSClient();
+
+var customer = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    Email = "email@email.com",
+    FullName = "Name Test",
+    DateOfBirth = new DateTime(2000, 1, 1),
+    GitHubUsername = "github-test"
+};
+
+var queryUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var sendMessageRequest = new SendMessageRequest
+{
+    QueueUrl = queryUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customer),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessageType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = nameof(CustomerCreated)
+            }
+        }
+    }
+};
+
+var response = await sqsClient.SendMessageAsync(sendMessageRequest);
+
+Console.WriteLine();
